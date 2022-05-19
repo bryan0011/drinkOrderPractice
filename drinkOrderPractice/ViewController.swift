0806660145b2fdeb6157bdbe3fa523201f95aaf0
Bryan Kuo <bryan0011@gmail.com>
2022-05-19 23:17:59 +0800

    Initial Commit

---
+++ drinkOrderPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drinkOrderPractice
+//
+//  Created by Bryan Kuo on 2022/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
